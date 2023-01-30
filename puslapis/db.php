f7db54da8d30effc90662f11d326baa4cef7c7ef
abcwers <abcwers@yahoo.com>
2023-01-30 19:48:15 +0200

    prideta keletas funkcijų prieš SQL_injection
    prideta keletas funkcijų prieš SQL_injection galimas atakas, tam tikrų simbolių pakeitimas HTML simboliais

--- puslapis/db.php
+++ puslapis/db.php
@@ -11,4 +11,23 @@ function getDBConnection(){
 	
 	return new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", "$db_user", "$db_password");
 }
+
+function selectData($query) {
+$statement = getDBConnection()->prepare($query);
+$statement->execute();
+$result = $statement->fetchAll();
+return $result;
+
+}
+
+function BlockSQLInjection($str){
+return str_replace(array("'",'"',"'",'"'), array("&#39;","&quot;","&#39;","&quot;"), $str);
+}
+
+function test_input($data){
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
